7e2272366af06ddd5d73db4519579ba1ecb4aab3
Nik <weinni2000@gmail.com>
2026-06-25 14:25:07 +0200

    Update 2026-06-25

--- minimal_owl/__manifest__.py
+++ minimal_owl/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "demo",
     "author": "mytime.click",
     "category": "Uncategorized",
-    "version": "18.0.1.0",
+    "version": "18.0.1.1",
     "license": "AGPL-3",
     "depends": [
         "base",
